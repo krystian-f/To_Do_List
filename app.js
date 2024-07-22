0de0ed1a4f7ff133055b20bc8acafa18ede46d83
krystian-f <krystianfpa@gmail.com>
2024-07-22 23:52:11 +0200

    Edit module

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import { addNewTask } from "./src/new_task.js";
 import { displayTasks } from "./src/display_tasks.js";
 import { deleteTask } from "./src/delete_task.js";
+import { editTask } from "./src/edit_task.js";
 
 document.addEventListener('DOMContentLoaded', () => {
   addNewTask();
   displayTasks();
   deleteTask();
+  editTask();
 })
---
+++ src/edit_task.js
@@ -0,0 +1,45 @@
+const editTask = function () {
+  console.log(`Edit`);
+  const tasksDB = JSON.parse(localStorage.getItem('tasksDB')) || [];
+
+  document.addEventListener('click', (e) => {
+    if(e.target.classList.contains('edit-btn')) {
+      const taskItem = e.target.closest('.task-item');
+      const taskId = parseInt(taskItem.dataset.id, 10);
+      const taskTextElement = taskItem.querySelector('.task-text');
+
+      if(e.target.textContent === 'Edit') {
+        //Edit mode
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.value = taskTextElement.textContent.trim();
+        input.classList.add('task-edit-input');
+        taskItem.insertBefore(input, taskTextElement);
+        taskTextElement.style.display = 'none';
+        e.target.textContent = 'Done';
+      } else if (e.target.textContent === 'Done') {
+        const input = taskItem.querySelector('.task-edit-input');
+        const newValue = input.value.trim();
+        taskTextElement.textContent = newValue;
+        taskTextElement.style.display = '';
+        taskItem.removeChild(input);
+        e.target.textContent = 'Edit';
+
+      // Update DB Local Storage
+      const updatedTasksDB = tasksDB.map((task) => {
+        if(task.id === taskId) {
+          task.value = newValue;
+        }
+
+        return task;
+      })
+
+      localStorage.setItem('tasksDB', JSON.stringify(updatedTasksDB));        
+      }
+
+    }
+  })
+
+}
+
+export { editTask}
