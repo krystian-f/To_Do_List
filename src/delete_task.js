ec137a9f0c045ccb3ac3da1c8be3e3c15442be90
krystian-f <krystianfpa@gmail.com>
2024-07-24 00:00:47 +0200

    Fixed delete task module

--- src/delete_task.js
+++ src/delete_task.js
@@ -1,26 +1,24 @@
-import { dragAndDrop } from "./drag_and_drop.js";
+import { dragAndDrop } from './drag_and_drop.js';
 
-const deleteTask = function() {
-  console.log('Delete');
-
+const deleteTask = function () {
+  console.log(`Delete`);
   document.addEventListener('click', (e) => {
     if (e.target.classList.contains('delete-btn')) {
       const taskItem = e.target.closest('.task-item');
       const taskId = parseInt(taskItem.dataset.id, 10);
       let tasksDB = JSON.parse(localStorage.getItem('tasksDB')) || [];
 
-      //Update DB
-      tasksDB = tasksDB.filter((task) => {
-        task.id !== taskId;
-      })
+      //UpdateDB
+      tasksDB = tasksDB.filter((task) => task.id !== taskId)
 
-      //Update Local Storage 
+      //Update LocalStorage
       localStorage.setItem('tasksDB', JSON.stringify(tasksDB));
 
-      // Remove task from DOM
+      //Remove task from DOM
       taskItem.remove();
     }
   })
 }
 
-export { deleteTask };
+export { deleteTask }
+
